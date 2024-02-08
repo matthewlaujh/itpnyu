abf4a20861242042264234c29e5369c3b4b1f33c
Matthew Lau <matt@matthewlaujh.com>
2024-02-08 00:23:04 -0500

    natureOfCode-w2

--- itpnyu-blog/src/views/Spring2024/ItpgGt2480TheNatureOfCode/Week2.js
+++ itpnyu-blog/src/views/Spring2024/ItpgGt2480TheNatureOfCode/Week2.js
@@ -19,91 +19,52 @@ const BlogPost = () => {
       <div>
         <Header>Week 1</Header>
         <ExternalText>
-          Using the random walker as a model, develop a sketch that experiments
-          with motion. Here are some ideas but you should feel free to develop
-          your own. Another way of thinking about the assignment is to apply the
-          rules of motion to another medium of expression: sound, color, number,
-          scale...
+          Find any sketch you previously created in p5.js with separate x,y
+          variables for elements on the sketch. Can you rewrite the sketch with
+          createVector() for each of these pairs? For example, rework your
+          sketch from week 1 and use vectors! Try incorporating the concept of
+          acceleration. Can you create a methodology for calculating a dynamic
+          acceleration, one that changes over time based on any number of
+          factors? What is a random "acceleration" walk?
         </ExternalText>
         <Text>
-          Was excited when Dan said that we could use this class to touch on
-          other programming languages and one method was to port code across
-          languages, my mind instantly went to Shaders (GLSL). Shaders have
-          always been this BANE OF MY EXISTENCE, back when I first started
-          working as a Creative Technologist at R/GA one of my very first
-          projects was to write a form of GLSL called SparkSL for some
-          backgrounds for an instagram filter and it was so hard to understand I
-          had to port from ShaderToy to GLSLto SparkSL and finding documentation
-          was so difficult. I managed to fumble my way through that by combining
-          a bunch of ShaderToy and Book Of Shader examples but I never really
-          understood what I was doing. So I thought this would be a great
-          opportunity to revisit that and try to understand it better.
+          I am still struggling trying to do GLSL natively but I hope I'll get
+          there at some point this semester but here's a sketch based on last
+          week's sketch but using createVector() and play around with
+          acceleration. I also changed the fragment shader colours to use random
+          noise as the texture.
           <br></br>
           <br></br>
-          I've also been super drawn to shaders as I've seen some really
-          beautiful generative graphics and art made with them. I love looking
-          at generative art and I think this might be my opportunity to get more
-          into that. Also because of the limitations of p5 and how sketches
-          start to get slow after leaving them to run for awhile, I thought it
-          would be a good idea to learn how to use shaders so that I could look
-          at creating more complex graphics without slowing down the sketch and
-          bringing my computer to a crawl.
+          As I was working on the sketch I realised that it look like dust or
+          like microorganisms moving around. Or like that one camera lens that I
+          messed up and let fungus grow inside, there's something interesting
+          about this micro random walk behaviour and the shapes it forms.
           <br></br>
           <br></br>
-          Ok those were initial excitements and thoughts but I've also just
-          spent the last 8 hours trying to figure out how to firstly, get GLSL
-          to run as a react component on my blog (which I have still not figured
-          out), secondly try to draw a moving dot on the screen and have it move
-          randomly (which I also have not figured out), and thirdly I've been
-          researching and I can't figure out if it is even possible to do most
-          of what I see on the nature of code syllabus using GLSL with my
-          limited knowledge. But time's running out and I need to start with
-          something manageable so I switched focus for the time being to
-          something simpler like using the random walker example and then try to
-          apply a shader to it to manipulate the colour.
-          <br></br>
-          <br></br>
-          Managed to cobble something together hectically and frantically
-          changing values in code and trying to undertsand them using the Random
-          Walker Class example with this tutorial from ITP Alumni on{" "}
-          <Links to="https://itp-xstory.github.io/p5js-shaders/#/./docs/setting-up-shaders-in-p5">
-            how to use shaders with p5.js
-          </Links>{" "}
-          and some colour examples from{" "}
-          <Links to="https://thebookofshaders.com/06/">
-            The Book of Shaders
-          </Links>
-          . Although I didn't quite manage to do what I wanted to do, I think
-          this is a good start? A cold and slow start but a start nonetheless!
-          Nothing fancy but I might need to rethink my plans to learn GLSL or
-          maybe find a more manageable way to learn it while learning the stuff
-          from the Nature of Code. Just not sure if I can do both at the same
-          time, would I be sacrificing one for the other?
-          <br></br>
-          <br></br>
-          Although not sure if it's just me but the colours from the Shaders
-          kinda look better than using RGB in p5.js? Or maybe I'm just blinded
-          by the fact that I managed to get it to work.
+          Also just because I was curious I left the sketch to run for about 30
+          minutes and surprisngly it did not lag up my whole computer and was
+          still running fine. Fans were whirring on my computer but the sketch
+          was still going strong! :)
         </Text>
         <Iframes
-          src="https://editor.p5js.org/matthewlaujh-itpnyu/full/CXNMEpVja"
-          title="p5.js sketch of https://editor.p5js.org/matthewlaujh-itpnyu/full/CXNMEpVja"
+          src="https://editor.p5js.org/matthewlaujh-itpnyu/full/9RA5mg2nK"
+          title="p5.js sketch of https://editor.p5js.org/matthewlaujh-itpnyu/sketches/9RA5mg2nK"
           height="50vh"
         />
+
         <Subheader>p5.js code for Walker Object</Subheader>
         <CodeSnippets
           language="javascript"
-          code={`// https://editor.p5js.org/matthewlaujh-itpnyu/full/CXNMEpVja
-// Based off the Random Walker Class from https://natureofcode.com/random/ and https://editor.p5js.org/natureofcode/sketches/5C69Xy
+          code={`// https://editor.p5js.org/matthewlaujh-itpnyu/sketches/9RA5mg2nK
+// Based off the Random Walker Class from https://natureofcode.com/random/ and https://editor.p5js.org/natureofcode/sketches/5C69XyrlsR
 
 // Shaders applied by me using https://itp-xstory.github.io/p5js-shaders/#/./docs/setting-up-shaders-in-p5 + https://github.com/aferriss/p5jsShaderExamples/blob/gh-pages/2_texture-coordinates/2-4_random/texcoord.vert
 
 // Shader colours applied using code adapted from https://thebookofshaders.com/06/
-
 let shaderCanvas;
 let shaderTexture;
 let walkers = [];
-let numWalkers = 100;
+let numWalkers = 1000;
 
 function preload() {
   shaderCanvas = loadShader('shader.vert', 'shader.frag');
@@ -137,137 +98,72 @@ function draw() {
   }
 }
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
 class Walker {
   constructor() {
-    this.x = random(width) - width / 2;
-    this.y = random(height) - height / 2;
-    this.size = 20;
+    this.pos = createVector(random(width) - width / 2, random(height) - height / 2);
+    this.vel = createVector(0, 0); // Start with zero velocity
+    this.acc = createVector(0, 0); // Start with zero acceleration
+    this.size = 0.5;
+  }
+
+  applyForce(force) {
+    this.acc.add(force); // Add the force to the acceleration
+  }
+
+  step() {
+    // Update acceleration randomly
+    let randomAcc = createVector(random(-0.5, 0.5), random(-0.5, 0.5));
+    this.applyForce(randomAcc);
+
+    // Update velocity
+    this.vel.add(this.acc);
+    this.vel.limit(0.5);
+    // Update position
+    this.pos.add(this.vel);
+
+    // Reset acceleration each frame
+    this.acc.mult(0);
   }
 
   show() {
     push();
     texture(shaderTexture);
-    ellipse(this.x, this.y, this.size, this.size);
+    ellipse(this.pos.x, this.pos.y, this.size, this.size);
     pop();
   }
-
-  step() {
-    this.xstep = random(-20, 20);
-    this.ystep = random(-20, 20);
-
-    this.x = lerp(this.x, this.x + this.xstep, 0.2);
-    this.y = lerp(this.y, this.y + this.ystep, 0.2);
-  }
-}
-
-`}
-        />
-
-        <Subheader>
-          GLSL code for Vertex Shader (Boilerplate to make GLSL work with p5.js){" "}
-        </Subheader>
-
-        <CodeSnippets
-          language="glsl"
-          code={`// https://editor.p5js.org/matthewlaujh-itpnyu/full/CXNMEpVja
-// From https://itp-xstory.github.io/p5js-shaders/#/./docs/setting-up-shaders-in-p5 + https://github.com/aferriss/p5jsShaderExamples/blob/gh-pages/2_texture-coordinates/2-4_random/texcoord.vert
-
-// Combined by me
-
-/*
-vert file and comments from adam ferriss
-https://github.com/aferriss/p5jsShaderExamples
-with additional comments from Louise Lessel
-*/ 
-
-
-// These are necessary definitions that let you graphics card know how to render the shader
-#ifdef GL_ES
-precision mediump float;
-#endif
-
-
-// This “vec3 aPosition” is a built in shader functionality. You must keep that naming.
-// It automatically gets the position of every vertex on your canvas
-
-attribute vec3 aPosition;
-
-// our texcoordinates
-attribute vec2 aTexCoord;
-
-// We always must do at least one thing in the vertex shader:
-// tell the pixel where on the screen it lives:
-
-// this is a variable that will be shared with the fragment shader
-// we will assign the attribute texcoords to the varying texcoords to move them from the vert shader to the frag shader
-// it can be called whatever you want but often people prefiv it with 'v' to indicate that it is a varying
-varying vec2 vTexCoord;
-
-void main() {
-  
-  // copy the texture coordinates
-  vTexCoord = aTexCoord;
-  
-  // copy the position data into a vec4, using 1.0 as the w component
-  vec4 positionVec4 = vec4(aPosition, 1.0);
-  
-  // Make sure the shader covers the entire screen:
-  // scale the rect by two, and move it to the center of the screen
-  // if we don't do this, it will appear with its bottom left corner in the center of the sketch
-  // try commenting this line out to see what happens
-  positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
-
-  // Send the vertex information on to the fragment shader
-  // this is done automatically, as long as you put it into the built in shader function “gl_Position”
-  gl_Position = positionVec4;
 }
 `}
         />
 
-        <Subheader>GLSL Code for Fragment Shader (Color Changing)</Subheader>
+        <Subheader>GLSL Code for Fragment Shader (Noise)</Subheader>
 
         <CodeSnippets
           language="glsl"
           code={`// https://editor.p5js.org/matthewlaujh-itpnyu/full/CXNMEpVja
-// Adapted from https://thebookofshaders.com
-
+// Adapted from https://thebookofshaders.com/10/
 #ifdef GL_ES
 precision mediump float;
 #endif
 
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 uniform float u_time;
 
-// Function from Iñigo Quiles
-// https://www.shadertoy.com/view/MsS3Wc
-vec3 hsb2rgb(in vec3 c){
-    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-                             6.0)-3.0)-1.0,
-                     0.0,
-                     1.0 );
-    rgb = rgb*rgb*(3.0-2.0*rgb);
-    return c.z * mix(vec3(1.0), rgb, c.y);
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
 }
 
 void main() {
-    // Define colorA and colorB in HSB space (Hue, Saturation, Brightness)
-    vec3 colorA = vec3(0.0, 1.0, 1.0);
-    vec3 colorB = vec3(1.0, 1.0, 0.5);
+    vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
-    // Sinwave time interpolation
-    float pct = abs(sin(u_time));
-    float alpha = abs(cos(u_time));
+    float rnd = random( st );
 
-    // Interpolate between the two HSB colors based on time
-    vec3 colorHSB = mix(colorA, colorB, pct);
-
-    // Convert the HSB to RGB
-    vec3 colorRGB = hsb2rgb(colorHSB);
-
-    gl_FragColor = vec4(colorRGB, alpha);
+    gl_FragColor = vec4(vec3(rnd),0.5);
 }
 `}
         />
