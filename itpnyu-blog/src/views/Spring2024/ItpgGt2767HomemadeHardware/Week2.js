ef3293240ba21b9aa95ac4a3d1f743f6e5bd7b5c
Matthew Lau <matt@matthewlaujh.com>
2024-02-09 01:21:32 -0500

    homemadeHardware-w2

--- itpnyu-blog/src/views/Spring2024/ItpgGt2767HomemadeHardware/Week2.js
+++ itpnyu-blog/src/views/Spring2024/ItpgGt2767HomemadeHardware/Week2.js
@@ -14,6 +14,78 @@ import P5Sketch from "../../../components/P5Sketch"
 import ImagesCarousel from "../../../components/ImagesCarousel"
 
 const BlogPost = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <div>
+        <Header>Week 2</Header>
+        <Subheader>
+          Homework - Programming Jig and Capacitive Sensor Controlling Multiple
+          LEDs.
+        </Subheader>
+        <ExternalText>
+          Finish making your ATtiny85 programming jig.
+          <br></br>
+          Breadboard a capacitive sensor controlling multiple LEDs.
+        </ExternalText>
+        <Text>
+          Finished soldering my programming jig and I think this might be one of
+          my better solder jobs in a long time and also tried to keep the wired
+          neat which I think worked out, overall it looks pretty good?
+          <br></br>
+          <br></br>
+          Made a simple capacitive sensor using a wire to control multiple LEDs,
+          although it's kind of finicky and sometimes I triggers both LEDs, I
+          suspect that it might be a shared ground issue but need to ask in
+          class about this. Also I've been obsessed with creating tiny circuits
+          in Electronics for Inventors so I thought I'd bring that spirit here
+          and try to create as small a footprint as possible, not sure if that's
+          also causing issues with the sensing.
+        </Text>
+        <VideoPlayer
+          src="https://itpnyublog.blob.core.windows.net/itpnyu-blog-spring2024-itpg-gt2767-homemadehardware-videos/itpg-gt-2767-homemadeHardware-weekTwo-touchLed.mp4"
+          alt="Description of video"
+        />
+        <Subheader>Touch Sensor Code.</Subheader>
+        <CodeSnippets
+          language="arduino"
+          code={`int touchPinA = A3;
+int touchPinB = A2;
+int valA = 0;
+int valB = 0;
+int ledPinA = 0;
+int ledPinB = 1;
+
+void setup() {
+  pinMode(ledPinA, OUTPUT);
+  pinMode(ledPinB, OUTPUT);
+}
+
+void loop() {
+  valA = analogRead(touchPinA);
+  valB = analogRead(touchPinB);
+  if (valA >= 200) {
+    digitalWrite(ledPinA, HIGH);
+  } else {
+    digitalWrite(ledPinA, LOW);
+  }
+
+  if (valB >= 200) {
+    digitalWrite(ledPinB, HIGH);
+  } else {
+    digitalWrite(ledPinB, LOW);
+  }
+}
+`}
+        />
+        <ImagesCarousel
+          images={[
+            "https://itpnyublog.blob.core.windows.net/itpnyu-blog-spring2024-itpg-gt2767-homemadehardware-images/itpg-gt-2767-homemadeHardware-weekTwo-programmgJigFront.jpg",
+            "https://itpnyublog.blob.core.windows.net/itpnyu-blog-spring2024-itpg-gt2767-homemadehardware-images/itpg-gt-2767-homemadeHardware-weekTwo-programmgJigBack.jpg",
+          ]}
+          orientation="landscape"
+        />
+      </div>
+    </Container>
+  )
 }
 export default BlogPost
